51f1f5ce07482c6388114af1eff461ef653bd624
conradlyn <32945493+conradlyn@users.noreply.github.com>
2024-07-02 16:23:36 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -15,8 +15,6 @@ git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
 git_sparse_clone main https://github.com/linkease/istore luci
 
 # Themes
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 
-# 更改 Argon 主题背景
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
